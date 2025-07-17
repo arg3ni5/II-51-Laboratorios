9f54033964f284606c4b5244e3823a13653eafc2
Fabian Chinchilla Mayorga <fb.argenis@gmail.com>
2025-07-16 20:33:30 -0600

    Laboratorio 7: Ejercicios de JS

---
+++ Lab07/script.js
@@ -0,0 +1,43 @@
+// Validación de formulario
+document.getElementById("validarBtn").addEventListener("click", function () {
+  const nombre = document.getElementById("nombre").value.trim();
+  const apellido = document.getElementById("apellido").value.trim();
+  const mensaje = document.getElementById("mensaje");
+
+  if (nombre === "" || apellido === "") {
+    mensaje.textContent = "Debe completar todos los campos.";
+    mensaje.className = "alert alert-danger";
+  } else {
+    mensaje.textContent = `Bienvenido, ${nombre} ${apellido}`;
+    mensaje.className = "alert alert-success";
+  }
+
+  Swal.fire(mensaje.textContent);
+});
+
+// jQuery: Agregar y quitar clase
+$("#agregarClase").click(function () {
+  $("#bloqueTexto").addClass("color");
+});
+
+$("#quitarClase").click(function () {
+  $("#bloqueTexto").removeClass("color");
+});
+
+// jQuery: Mostrar y ocultar elemento
+$("#mostrarOcultar").click(function () {
+  $("#elementoOculto").toggle();
+});
+
+let temaOscuro = false;
+
+function alternarTema() {
+  temaOscuro = temaOscuro ? false : true;
+  if (!temaOscuro) {
+    document.getElementById("tema").className = "fa-solid fa-moon";
+  }
+  else {
+    document.getElementById("tema").className = "fa-solid fa-sun";
+  }
+  document.getElementById("main").classList.toggle("oscuro");
+}
